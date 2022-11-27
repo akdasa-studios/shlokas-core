e817ea04aac4011ef5baa637bc725e6d4953ed08
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-11-27 14:25:02 +0000

    last commit before separation

--- tests/unit/domain/models/verse/Verse.spec.ts
+++ tests/unit/domain/models/verse/Verse.spec.ts
@@ -1,22 +1,25 @@
 import { Language } from '@lib/models'
-import { Verse, VerseId, VerseNumber, Translation, Transliteration, Synonym } from '@lib/models/verse'
+import { Verse, VerseId, VerseNumber, Translation, Transliteration, Synonym } from '@/models/verse'
 
 describe('Verse', () => {
   describe('constructor', () => {
     it('should return a new instance of Verse class', () => {
       expect(new Verse(
         new VerseId(),
-        new VerseNumber(['1']),
+        new VerseNumber(['BG', '1', '1']),
         new Language('EN', 'English'),
         new Transliteration([
-          'In the beginning God created the heavens and the earth.',
-          'Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.',
+          "dehino ’smin yathā dehe",
+          "kaumāraṁ yauvanaṁ jarā",
+          "tathā dehāntara-prāptir",
+          "dhīras tatra na muhyati"
         ]),
-        new Translation('In the beginning God created the heavens and the earth.'),
+        new Translation(
+          'As the embodied soul continuously passes, in this body, from boyhood to youth to old age, the soul similarly passes into another body at death. A sober person is not bewildered by such a change.'),
         [
-          new Synonym('God', 'Allah'),
-          new Synonym('heavens', 'sky'),
-          new Synonym('earth', 'land'),
+          new Synonym('dehinaḥ', 'of the embodied'),
+          new Synonym('asmin', 'in this;'),
+          new Synonym('yathā', 'as'),
         ]
       )).toBeInstanceOf(Verse)
     })
